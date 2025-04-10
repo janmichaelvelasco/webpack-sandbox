c511987c9d552d6a33b2f1a6bd0ac86adf27f97a
janmichaelvelasco <janmichael.velasco@jaybro.com.au>
2025-04-10 11:37:19 +1000

    feat: add Sass support and update project structure
    - Updated package.json to include Sass-related dependencies (sass, sass-loader, style-loader, css-loader).
    - Added a new SCSS file for styling with a simple body color.
    - Imported the SCSS file in the main JavaScript entry point.
    - Modified webpack.config.js to include rules for processing SCSS files.
    - Updated index.html to include a header for better visibility of the application.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   devServer: {
     static: './public',
     port: 3000,
